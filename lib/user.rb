b94443dd5d1d7439d4fa77738e9986ff78c12a3d
Stacy <lundquist.stacy@gmail.com>
2020-10-08 15:01:03 -0700

    reset user

--- lib/user.rb
+++ lib/user.rb
@@ -1,44 +1,33 @@
 require_relative 'recipient'
-
-
 class User < Recipient
-    attr_reader :real_name, :status_text, :status_emoji
-
-    def initialize(name:, slack_id:, real_name:, status_text:, status_emoji:)
-      super(name: name, slack_id: slack_id)
-      @real_name = real_name
-      @status_text = status_text
-      @status_emoji = status_emoji
+  attr_reader :real_name, :status_text, :status_emoji
+  def initialize(name:, slack_id:, real_name:, status_text:, status_emoji:)
+    super(name: name, slack_id: slack_id)
+    @real_name = real_name
+    @status_text = status_text
+    @status_emoji = status_emoji
+  end
+  def details
+    #prints info for currently selected user, if not, should let user know and return to main command prompt
+    detailed_info = "Slack_id: #{@slack_id}, Name: #{@name}"
+    return detailed_info
+  end
+  def self.list_all
+    users = []
+    # get method defined in recipient.rb
+    response = get(USER_LIST, {token: ENV['SLACK_TOKEN']})
+    response["members"].each do |user|
+      new_user = new(
+          name: user["name"],
+          slack_id: user["id"],
+          real_name: user["profile"]["real_name"],
+          status_text: user["profile"]["status_text"],
+          status_emoji: user["profile"]["status_emoji"]
+      )
+      users << new_user
     end
-
-
-    def details
-      #prints info for currently selected user, if not, should let user know and return to main command prompt
-      detailed_info = "Slack_id: #{@slack_id}, Name: #{@name}"
-      return detailed_info
-    end
-
-    def self.list_all
-      users = []
-      # get method defined in recipient.rb
-      response = get(USER_LIST, {token: ENV['SLACK_TOKEN']})
-
-      response["members"].each do |user|
-        new_user = new(
-            name: user["name"],
-            slack_id: user["id"],
-            real_name: user["profile"]["real_name"],
-            status_text: user["profile"]["status_text"],
-            status_emoji: user["profile"]["status_emoji"]
-        )
-        users << new_user
-      end
-      return users
-    end
+    return users
+  end
 end
-
-
-
-
 # pp data = self.get('https://slack.com/api/users.list', query: {token: ENV['SLACK_TOKEN']})
 # p use1 = User.new(real_name: "mona", status_text: "test", status_emoji: "happy", name: "test2", slack_id: 5)
